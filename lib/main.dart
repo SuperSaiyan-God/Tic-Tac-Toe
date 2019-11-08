3c126c17b838dc2d37a756cf322766541946a0f1
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2019-11-08 21:37:25 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/homePage.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
